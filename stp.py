082352b0424d1f2bd61691a33a51b8da02699e48
Maen Artimy <artimym@gmail.com>
2023-04-15 17:16:52 -0300

    improve code

--- stp.py
+++ stp.py
@@ -1,51 +1,59 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 13 08:37:16 2023
+Copyright 2023 Maen Artimy
 
-@author: artim
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 import networkx as nx
 
 MAX_INT = 2**63-1
 
+
 def get_stp(G):
     """
-    Find the IEEE Spanning Tree Protocol
+    Apply the IEEE Spanning Tree Protocol for a graph G using ID and
+    weight attributes of the nodes and edges, respectively.
     """
 
     # Find the root node with the smallest ID
     root_id = min(G.nodes, key=lambda n: G.nodes[n]["ID"])
 
-    # Cache the shortest path length from the root node
-    if "distances" not in G.graph:
-        G.graph["distances"] = nx.shortest_path_length(G, source=root_id)
-    distances = G.graph["distances"]
+    # Find the shortest path length from the root node
+    distances = nx.shortest_path_length(G, source=root_id)
 
     # Sort nodes based on their distance from the root
     sorted_nodes = sorted(G.nodes, key=lambda n: distances[n])
 
     # Find the shortest path to the root node for each node in order of distance
-    tree = []
+    short_routes = []
     for node in sorted_nodes:
-        paths = nx.all_shortest_paths(G, node, root_id, weight='weight')
+        paths = list(nx.all_shortest_paths(G, node, root_id, weight='weight'))
 
-        selected_path = None
-        short = MAX_INT
-        for path in paths:
-            if len(path) > 1:
-                if G.nodes[path[1]]["ID"] < short:
-                    selected_path = path
-                    short = G.nodes[path[1]]["ID"]
-            else:
+        # If there are more than one equal path, choose the path through the
+        # the switch with lowest ID
+        selected_path = paths[0]
+        for path in paths[1:]:
+            if path < selected_path:
                 selected_path = path
-        tree.append(selected_path)
+        short_routes.append(selected_path)
 
-    # Create a new graph for the STP
-    spanning = nx.Graph(root=root_id)
-    for branch in tree:
-        if len(branch) > 1:
-            s, t = branch[0:2]
-            spanning.add_edge(s, t)
+    # Create a new graph for the spanning tree
+    st = nx.Graph(root=root_id)
 
-    return spanning
+    # The first two nodes in each path form an edges in the STP.
+    # Ignore the first path which includes only the root
+    edges = [tuple(r[0:2]) for r in short_routes[1:]]
+    st.add_edges_from(edges)
+
+    return st
